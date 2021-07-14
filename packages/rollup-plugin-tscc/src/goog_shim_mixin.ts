0b5d02e554140bd41973e5a81cf72e8ea81acd72
theseanl <i73hi64d0wr5df8pckig@gmail.com>
2021-07-14 22:30:12 +0900

    fix: remove debug artifact, polish variable name

--- packages/rollup-plugin-tscc/src/goog_shim_mixin.ts
+++ packages/rollup-plugin-tscc/src/goog_shim_mixin.ts
@@ -9,7 +9,7 @@ import * as path from 'path';
 
 const SHIM_ROOT = path.resolve(__dirname, "../third_party/closure_library");
 
-const things = new Map([
+const moduleNameToShim = new Map([
 	["goog:goog", fs.readFileSync(path.join(SHIM_ROOT, "goog_shim.js"), "utf8")],
 	["goog:goog.reflect", fs.readFileSync(path.join(SHIM_ROOT, "reflect_shim.js"), "utf8")]
 ]);
@@ -20,11 +20,11 @@ const PREFIX = "\0tscc\0";
 export function googShimMixin(plugin: Plugin): Plugin {
 	const {resolveId, load} = plugin;
 	plugin.resolveId = function (id, importer) {
-		if (things.has(id)) return PREFIX + id;
+		if (moduleNameToShim.has(id)) return PREFIX + id;
 		return Reflect.apply(resolveId!, this, arguments);
 	}
 	plugin.load = function (id) {
-		if (id.startsWith(PREFIX)) return things.get(id.substring(PREFIX.length));
+		if (id.startsWith(PREFIX)) return moduleNameToShim.get(id.substring(PREFIX.length));
 		return Reflect.apply(load!, this, arguments);
 	}
 	return plugin;
